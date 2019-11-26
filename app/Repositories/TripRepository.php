cc2ba6ee960de2e0ee4621f56a6366a97e8f2888
yasseen semlali <ec2-user@ip-172-31-40-124.ec2.internal>
2019-11-26 15:25:10 +0000

    fixed url bug

--- app/Repositories/TripRepository.php
+++ app/Repositories/TripRepository.php
@@ -4,8 +4,14 @@
 
     use App\Trip;
     use App\User;
+use App\Repositories\HereRepository;
 
     class TripRepository {
+        private $here;
+        
+        public function __construct(HereRepository $here) {
+            $this->here = $here;
+        }
         
         //tempoprary, make it take id as param later
         public function getAllTrips(int $paginate, int $id = -1) {
@@ -16,12 +22,19 @@
             }
             
             $trips = $user->trips()->orderBy('created_at')->paginate($paginate);
-            
+            var_dump($trips->toArray());
             return $trips;
         }
         
-        public function addTrip(int $id) {
+        public function addTrip(float $fromLatitude, float $fromLongtude, float $toLatitude, float $toLongtude, string $transportType, string $fuelType = null, float $fuelConsumption = null, int $id = -1) {
+            if($id == -1) {
+                $user = Auth::user();
+            } else {
+                $user = User::findOrFail($id);
+            }
             
+            $trip = $this->here->getTrip($fromLatitude, $fromLongtude, $toLatitude, $toLongtude, $transportType, $fuelType, $fuelConsumption);
+            var_dump($trip);
         }
     }
 ?>

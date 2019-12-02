2f8ddfed74a008f27ac5dc0845bc056686ed627e
EC2 Default User <ec2-user@ip-172-31-88-118.ec2.internal>
2019-12-02 18:27:47 +0000

    .

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -104,6 +104,7 @@ class ApiController extends Controller
     	if (!$user)
     	 return response()->json(['error' => 'invalid_token'], 401);
     	else {
+    	    echo 'test1';
     	    $fromlatitude = $request->query('fromlatitude');
     	    $fromlongitude = $request->query('fromlongitude');
     	    $tolatitude = $request->query('tolatitude');
@@ -111,6 +112,7 @@ class ApiController extends Controller
     	    $mode = $request->query('mode');
     	    $engine = $request->query('engine');
     	    $consumption = $request->query('consumption');
+    	    echo 'test2';
     	    
     	    if(!isset($fromlatitude)) {
     	        $err[] = 'fromlatitude must be set';
@@ -143,6 +145,7 @@ class ApiController extends Controller
     	                'errors' => $err
     	            ], 422);
     	    }
+    	    echo 'test3';
     	    
     	    $trip = $this->trip->addTrip($user, $fromlatitude, $fromlongitude,$tolatitude ,$tolongitude ,$mode ,$engine, $consumption );
     	    
@@ -158,6 +161,7 @@ class ApiController extends Controller
     		$response['distance'] = $trip->distance;
     		$response['traveltime'] = $trip->travelTime;
     		$response['co2emissions'] = $trip->co2emissions;
+    	    echo 'test4';
     		    
     		    
     		return response()->json($response);
